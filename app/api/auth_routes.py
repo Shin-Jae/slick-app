fa54c56376ea7d9b0e3289c252c7e1487518a460
Jae Shin <7jaeshin@gmail.com>
2022-06-25 15:34:59 -0400

    sign up form changed

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -3,6 +3,7 @@ from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+import datetime
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -63,9 +64,12 @@ def sign_up():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user = User(
-            username=form.data['username'],
+            first_name=form.data['first_name'],
+            last_name=form.data['last_name'],
             email=form.data['email'],
-            password=form.data['password']
+            profile_img=form.data['profile_img'],
+            password=form.data['password'],
+            created_at=datetime.datetime.now()
         )
         db.session.add(user)
         db.session.commit()
