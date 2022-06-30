c8464808a08ce1de883299b92f0f5dc3b5755b8f
Walker Adkins <walkeradkins@gmail.com>
2022-06-30 12:09:22 -0500

    some styling and error handling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,63 +4,61 @@ import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
+// import Route from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 import MainPage from './components/Mainpage';
 import Channels from './components/Mainpage/Channels';
 import PrivateRoute from './components/PrivateRoute';
+import PageNotFound from './components/PageNotFound';
 // import Chat from './components/TestChat'
 
 function App() {
-	const [loaded, setLoaded] = useState(false);
-	const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		(async () => {
-			await dispatch(authenticate());
-			setLoaded(true);
-		})();
-	}, [dispatch]);
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
 
-	if (!loaded) {
-		return null;
-	}
+  if (!loaded) {
+    return null;
+  }
 
-	return (
-		<BrowserRouter>
-			<NavBar />
-			<Switch>
-				<Route path='/login' exact={true}>
-					<LoginForm />
-				</Route>
-				<Route path='/sign-up' exact={true}>
-					<SignUpForm />
-				</Route>
-				<PrivateRoute>
-					<Switch >
-						<ProtectedRoute exact path='/users/:userId'>
-							<MainPage />
-							{/* <Chat /> */}
-						</ProtectedRoute>
-						<ProtectedRoute path='/users/:userId/:channelId'>
-							<Channels />
-						</ProtectedRoute>
-					</Switch>
-					<ProtectedRoute path='/users' exact={true} >
-						<UsersList />
-					</ProtectedRoute>
-					<ProtectedRoute path='/user/:userId' exact={true} >
-						<User />
-					</ProtectedRoute>
-					<ProtectedRoute path='/' exact={true} >
-						<h1>My Home Page</h1>
-					</ProtectedRoute>
-				</PrivateRoute>
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <NavBar loaded={loaded} />
+      <Route exact path='/login'>
+        <LoginForm />
+      </Route>
+      <Route exact path='/sign-up'>
+        <SignUpForm />
+      </Route>
+      <PrivateRoute>
+        <Switch>
+          <Route exact path='/users/:userId'>
+            <MainPage />
+          </Route>
+          <Route exact path='/users/:userId/:channelId'>
+            <Channels />
+          </Route>
+          <Route exact path='/users' >
+            <UsersList />
+          </Route>
+          <Route exact path='/user/:userId'>
+            <User />
+          </Route>
+          <Route>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </PrivateRoute>
+    </BrowserRouter >
+  );
 }
 
 export default App;
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,56 +5,31 @@ import LogoutButton from './auth/LogoutButton';
 import './NavBar.css'
 import Search from './Search';
 
-const NavBar = () => {
-	const user = useSelector((state) => state.session.user)
+const NavBar = ({ loaded }) => {
+  const user = useSelector((state) => state.session.user)
 
-	if (!user) {
-		return (
-			<nav className='navbar'>
-				{/* <p>Logged-in: {`user.id: ${id} name: ${first_name} ${last_name}`}</p> */}
-				<ul>
-					<li>
-						{/* <NavLink to='/' exact={true} activeClassName='active'>
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/login' exact={true} activeClassName='active'>
-							Login
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/sign-up' exact={true} activeClassName='active'>
-							Sign Up
-						</NavLink> */}
-					</li>
-				</ul>
-			</nav>
-		);
-	} else {
-		const { id, first_name, last_name } = user;
-		return (
-			<nav className='navbar'>
-				<p>Logged-in: {`id: ${id}, ${first_name} ${last_name}`}</p>
-				<Search />
-				<ul>
-					<li>
-						<NavLink to='/users' exact={true} activeClassName='active'>
-							Users
-						</NavLink>
-					</li>
-					{/* <li>
-						<NavLink to={`/users/${id}`} exact={true} activeClassName='active'>
-							Main Page
-						</NavLink>
-					</li> */}
-					<li>
-						<LogoutButton />
-					</li>
-				</ul>
-			</nav>
-		)
-	}
+  let sessionNav;
+
+  if (user) {
+    const { id, first_name, last_name } = user;
+    sessionNav = (
+      <>
+        <p>Logo HERE</p>
+        <p>Logged-in: {`id: ${id}, ${first_name} ${last_name}`}</p>
+        <Search />
+        <LogoutButton />
+      </>
+    )
+  } else {
+    sessionNav = (
+      <h2>Logo Here</h2>
+    )
+  }
+  return (
+    <nav className='navbar'>
+      {loaded && sessionNav}
+    </nav>
+  )
 }
 
 export default NavBar;
