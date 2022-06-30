09b17094328841df5762e164a566867b564c49eb
Dayton Chen <daytonchen120@gmail.com>
2022-06-30 14:40:55 -0700

    favicon and logo added

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -4,18 +4,28 @@ import { useSelector } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
 import './NavBar.css'
 import Search from './Search';
+import { useHistory } from 'react-router-dom';
+import slickicon from "../images/slickicon.png"
 
 const NavBar = ({ loaded }) => {
   const user = useSelector((state) => state.session.user)
-
+  const history = useHistory()
   let sessionNav;
-
+  const homePage = () => {
+    history.push(`/users/${user.id}`)
+  }
   if (user) {
     const { id, first_name, last_name } = user;
     sessionNav = (
       <>
-        <p>Logo HERE</p>
-        <p>Logged-in: {`id: ${id}, ${first_name} ${last_name}`}</p>
+        {/* <p>Logo HERE</p> */}
+        <img 
+        className='slick-logo'
+        style={{ cursor: "pointer" }}
+        alt=''
+        src={slickicon}
+        onClick={homePage}></img>
+        <p>Welcome {`${first_name} ${last_name}!`}</p>
         <Search />
         <LogoutButton />
       </>
