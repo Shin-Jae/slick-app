4116745d719ae8359fa1b699f979e213e8f8b5ae
Walker Adkins <walkeradkins@gmail.com>
2022-06-29 20:46:22 -0500

    some button style

--- react-app/src/components/CreateDMs/index.js
+++ react-app/src/components/CreateDMs/index.js
@@ -3,19 +3,24 @@ import React, { useState } from "react";
 import CreateDMForm from "./CreateDMsForm";
 
 const CreateDMModal = () => {
-    const [showModal, setShowModal] = useState(false);
-    return (
-        <>
-            <div>
-                <button onClick={() => setShowModal(true)}>Create DM</button>
-                {showModal && (
-                    <Modal onClose={() => setShowModal(false)}>
-                        <CreateDMForm onClose={() => setShowModal(false)} />
-                    </Modal>
-                )}
-            </div>
-        </>
-    )
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <div>
+        <button
+          className='button__create-channel'
+          onClick={() => setShowModal(true)}
+        >
+          <span class="material-symbols-outlined">add</span>
+        </button>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <CreateDMForm onClose={() => setShowModal(false)} />
+          </Modal>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default CreateDMModal
