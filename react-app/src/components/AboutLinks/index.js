9c5950d7bc189e3dad010aaca98db0052491d4d3
Jae Shin <7jaeshin@gmail.com>
2022-12-19 18:16:30 -0500

    update

--- react-app/src/components/AboutLinks/index.js
+++ react-app/src/components/AboutLinks/index.js
@@ -6,15 +6,15 @@ const AboutLinks = () => {
     <div className='about__links--container'>
 
       <a className='about__links--text' href='https://www.github.com/Shin-Jae' target="_blank">
-        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'}/>
+        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'} />
         Jae Shin
       </a>
       <a className='about__links--text' href='https://www.github.com/spursforever' target="_blank">
-        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'}/>
+        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'} />
         Dayton Chen
       </a>
       <a className='about__links--text' href='https://www.github.com/walkeradkins' target="_blank">
-        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'}/>
+        <FaGithubSquare className='github__icon' color={'rgb(51, 51, 51)'} />
         Walker Adkins
       </a>
     </div>
@@ -22,3 +22,4 @@ const AboutLinks = () => {
 }
 
 export default AboutLinks;
+//
